c296377838435ad9bc0d96eff0931feca4f7bfd0
MohamedWasfy <s-mohamed.safieddin@zewailcity.edu.eg>
2017-08-23 17:53:24 +0200

    Update enemy.h
    class header

--- enemy.h
+++ enemy.h
@@ -1,6 +1,59 @@
 #ifndef ENEMY_H
 #define ENEMY_H
-struct enemy{
-    float priority;
+
+
+class enemy
+{
+    public:
+        enemy();
+        virtual ~enemy();
+
+        int getSequence();
+        void setSequence(int s);
+
+        int getTime();
+        void setTime(int t);
+
+        int getHealth();
+        void setHealth(int h);
+
+        int getPower();
+        void setPower(int p);
+
+        int getPeriod();
+        void setPeriod(int pr);
+
+        char getType();
+        void setType(char ty);
+
+        char getRegion();
+        void setRegion(char R);
+
+        int getDistance();
+        void setDistance(int D);
+
+        float getPriority();
+        void calcPriority();
+
+        void setPriorityParams(int c1,int c2,int c3);
+
+        void updateVars(); // to update distance and priority according to time steps
+
+
+    protected:
+
+    private:
+        int c1,c2,c3; // priority prarameters
+        int s; // sequence
+        int t; // time
+        int h; //health
+        int p; //power
+        int pr; //hit period
+        char ty,R; // type and region
+        int D=60; //distance
+        float priority;
+
+
 };
+
 #endif // ENEMY_H
